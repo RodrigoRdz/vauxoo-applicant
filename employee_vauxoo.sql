b421b615f6eee017eb93d64dff514692c1f94171
RodrigoRdz <rodrigo_132013@hotmail.com>
2017-05-29 02:55:38 -0500

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,13 +3,43 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
+CREATE TABLE employee (id integer NOT NULL,
+  first_name character varying(10),
+  last_name character varying(10),
+  department_id integer,
+  id_hobby_1 integer,
+  id_hobby_2 integer,
+  id_jefe integer,
+  CONSTRAINT employee_pkey PRIMARY KEY (id),
+  CONSTRAINT employee_department_fkey FOREIGN KEY (department_id)
+      REFERENCES employee_department (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT employee_id_hobby_1_fkey FOREIGN KEY (id_hobby_1)
+      REFERENCES employee_hobby (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT employee_id_hobby_2_fkey FOREIGN KEY (id_hobby_2)
+      REFERENCES employee_hobby (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT employee_id_jefe_fkey FOREIGN KEY (id_jefe)
+      REFERENCES employee (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+  
+  
 );
 
-CREATE TABLE employee_department (
+CREATE TABLE employee_department ( id integer NOT NULL,
+  name character varying(15),
+  description character varying(20),
+  CONSTRAINT employee_department_pkey PRIMARY KEY (id)
+  
+  
 );
 
 CREATE TABLE employee_hobby (
+  id integer NOT NULL,
+  name character varying(15),
+  description character varying(25),
+  CONSTRAINT employee_hobby_pkey PRIMARY KEY (id)
 );
 
 -- ...
