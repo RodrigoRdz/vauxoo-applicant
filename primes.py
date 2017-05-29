2b1e0ae19fa8af318ef12dcc879afcb926db1313
RodrigoRdz <rodrigo_132013@hotmail.com>
2017-05-29 03:02:17 -0500

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,15 +2,25 @@
 Your module documentation here
 """
 
+class PrimeClass:
 
-class PrimeClass(object):
     """
     Your class documentation here
     """
-
-    def is_prime(self, num_int):
-        """
+ def IsPrime(self,num_int):
+       """
         Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        """ 
+
+  num_int=int(input("ingrese un numero\n"))  
+  if(num_int%2==0):
+          print(True)
+           
+
+  else:
+           print(False)
+           
+
+        
+obj = PrimeClass()
+print(obj.IsPrime(0))
