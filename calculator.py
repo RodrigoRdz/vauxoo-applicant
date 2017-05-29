c9df65fe6bb03e5eb92cc05e4f70ed5c64f268ee
RodrigoRdz <rodrigo_132013@hotmail.com>
2017-05-29 03:50:55 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,18 @@
 class CalculatorClass:
-      def sum(self,num_list):
-     list = []
-     suma = 0
+    def sum(self,num_list):
+      list = []
+      suma = 0
 
-     for i in range(5):
-     num_list=int(input("ingrese un numero\n"))  
-     list.append(num_list)
+      for i in range(5):
+       num_list=int(input("ingrese un numero\n"))  
+       list.append(num_list)
 
-     for j in list:
-     suma += int(j)
 
-     print('La suma es:',suma)
+
+      for j in list:
+       suma += int(j)
+
+      print('La suma es:',suma)
 
 
 obj = CalculatorClass()
